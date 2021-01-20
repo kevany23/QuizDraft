64de0037bebaf360f73a6b6d6ecfd57c2c7d5793
kevany23 <kevan23yang@gmail.com>
2021-01-19 21:47:44 -0800

    Moved ejs rendering to separate router

--- server/app.js
+++ server/app.js
@@ -3,86 +3,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 console.log("PORT: " + port);
 const Database = require('./database');
+const client = require('./client');
 app.use(express.urlencoded({extended: true}));
-
-console.log(process.argv);
-console.log(process.argv.length);
+app.use('/client', client);
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('home.ejs');
-})
-
-app.get('/quiz/:id', async (req, res) => {
-  // lookup quiz info
-  let id = req.params.id;
-  try {
-    let quiz = await Database.Quiz.findById(id);
-    await Database.getQuizQuestions(quiz);
-    if (!quiz) {
-      throw new Error();
-    }
-    res.render('quiz.ejs', {quiz: quiz});
-  } catch (err) {
-    res.status(404).send("Not found.")
-  }
-})
-
-app.post('/createQuiz', async (req, res) => {
-  let { name, subject } = req.body;
-  let quiz = Database.Quiz({
-    name: name,
-    subject: subject
-  });
-  try {
-    let result = await quiz.save();
-    console.dir(result);
-    res.redirect('/quiz/' + result._id);
-  } catch (err) {
-    res.status(404).send("Quiz Creation Error");
-  }
-})
-
-app.post('/editQuiz/:id', async (req, res) => {
-  let id = req.params.id;
-  let { name, subject } = req.body;
-  try {
-    let quizLookup = await Database.Quiz.findByIdAndUpdate(id, {
-      name: name,
-      subject: subject
-    });
-    res.redirect(`/quiz/${id}`);
-  } catch(err) {
-    res.status(404).send("Quiz Edit Error");
-  }
-})
-
-app.post('/addQuestion/:id', async (req, res) => {
-  let id = req.params.id;
-  let { question, answer } = req.body;
-  let quiz = await Database.Quiz.findById(id);
-  let quizQuestion = new Database.QuizQuestion({
-    quizId: id,
-    question: question,
-    answer: answer
-  });
-  try {
-    let result = await quizQuestion.save();
-  } catch(err) {
-    res.status(404).send("Add question error");
-  }
-  res.redirect('back');
-})
-
-app.get('/getQuizzes', async (req, res) => {
-  let quizList = await Database.Quiz.find();
-  //console.log(quizList);
-  try {
-    res.render('quizlist.ejs', { quizList: quizList });
-  } catch(err) {
-    res.status(404).send("Not found");
-  }
+  res.redirect('/client');
 })
 
 app.listen(port, () => {
---
+++ server/client.js
@@ -0,0 +1,85 @@
+/**
+ * This is the router for rendering ejs pages from the client for
+ * server testing pages.
+ */
+
+ const express = require('express');
+ const Database = require('./database');
+ const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('home.ejs');
+})
+
+router.get('/quiz/:id', async (req, res) => {
+  // lookup quiz info
+  let id = req.params.id;
+  try {
+    let quiz = await Database.Quiz.findById(id);
+    await Database.getQuizQuestions(quiz);
+    if (!quiz) {
+      throw new Error();
+    }
+    res.render('quiz.ejs', {quiz: quiz});
+  } catch (err) {
+    res.status(404).send("Not found.")
+  }
+})
+
+router.post('/createQuiz', async (req, res) => {
+  let { name, subject } = req.body;
+  let quiz = Database.Quiz({
+    name: name,
+    subject: subject
+  });
+  try {
+    let result = await quiz.save();
+    console.dir(result);
+    res.redirect('/client/quiz/' + result._id);
+  } catch (err) {
+    res.status(404).send("Quiz Creation Error");
+  }
+})
+
+router.post('/editQuiz/:id', async (req, res) => {
+  let id = req.params.id;
+  let { name, subject } = req.body;
+  try {
+    let quizLookup = await Database.Quiz.findByIdAndUpdate(id, {
+      name: name,
+      subject: subject
+    });
+    res.redirect(`/client/quiz/${id}`);
+  } catch(err) {
+    res.status(404).send("Quiz Edit Error");
+  }
+})
+
+router.post('/addQuestion/:id', async (req, res) => {
+  let id = req.params.id;
+  let { question, answer } = req.body;
+  let quiz = await Database.Quiz.findById(id);
+  let quizQuestion = new Database.QuizQuestion({
+    quizId: id,
+    question: question,
+    answer: answer
+  });
+  try {
+    let result = await quizQuestion.save();
+  } catch(err) {
+    res.status(404).send("Add question error");
+  }
+  res.redirect('back');
+})
+
+router.get('/getQuizzes', async (req, res) => {
+  let quizList = await Database.Quiz.find();
+  //console.log(quizList);
+  try {
+    res.render('quizlist.ejs', { quizList: quizList });
+  } catch(err) {
+    res.status(404).send("Not found");
+  }
+})
+
+module.exports = router;
